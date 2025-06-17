3ad25e0fff879834ae41ce187caf24b78c3564ae
Wafae haida <95100809+wafae-haida@users.noreply.github.com>
2025-06-17 11:01:40 +0100

    Mise à jour

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
-// tailwind.config.js
-  module.exports = {
-   purge: [],
-   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  }
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
